af8ba69c8719b416610a75beb05d238e7020b635
shriniket <shriniketkulkarni42@gmail.com>
2023-12-19 20:00:07 +0530

    cors

--- api/index.js
+++ api/index.js
@@ -16,7 +16,9 @@ const PORT = process.env.PORT || 4000;
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.JWT_SECRET || "defaultSecret"; // Use a default secret if not provided in the environment
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({ credentials: true, origin: "https://snk-mern-blog.vercel.app/" })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
