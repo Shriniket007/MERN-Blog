bcb7467ca05e388bf5d0ef58d0321869a53f21d8
shriniket <shriniketkulkarni42@gmail.com>
2023-12-25 14:07:27 +0530

    cors

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -21,7 +21,6 @@ export default function CreatePost() {
       method: "POST",
       body: data,
       credentials: "include",
-      mode: "no-cors",
     });
     if (response.ok) {
       setRedirect(true);
